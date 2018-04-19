e274dc3db6b0b313ecf356da46b90955d1e0057b
riccoarntz <ricco@mediamonks.com>
2018-04-19 14:58:47 +0200

    include abstractComponentMixin within AbstractTransitionBlock.

--- tool/scaffold/src/app/component/block/AbstractTransitionBlock.ts
+++ tool/scaffold/src/app/component/block/AbstractTransitionBlock.ts
@@ -1,9 +1,10 @@
 import CoreComponent from 'muban-core/lib/CoreComponent';
 import { mubanTransitionCoreMixin, mubanTransitionMixin, IMubanTransitionMixin } from 'muban-transition-component';
 import abstractBlockMixin from 'app/component/block/AbstractBlockMixin';
+import abstractComponentMixin from 'app/component/AbstractComponentMixin';
 
 export const base: Constructor<IMubanTransitionMixin> &
-  typeof CoreComponent = abstractBlockMixin(mubanTransitionMixin(mubanTransitionCoreMixin(CoreComponent)));
+  typeof CoreComponent = abstractComponentMixin(abstractBlockMixin(mubanTransitionMixin(mubanTransitionCoreMixin(CoreComponent))));
 export default class AbstractTransitionBlock extends base {
 
 }
